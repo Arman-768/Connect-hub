b720bf803c7f41ca7ebfa785a764a2ba7cdd6004
Arman-768 <155524903+Arman-768@users.noreply.github.com>
2025-08-19 23:37:43 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Navigate, Route,Routes} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import LoginPage from './pages/LoginPage'
+import {Toaster} from "react-hot-toast";
+import { useContext } from 'react'
+import { Authcontext } from '../context/storeContext'
+
+const App = () => {
+  const {authUser}=useContext(Authcontext);
+  return (
+    <div  className="bg-[url('/bgImage.svg')] bg-contain">
+      <Toaster/>
+      <Routes>
+          <Route path='/' element={authUser ? <HomePage/> : <Navigate to='/login'/>}/>
+          <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to='/'/>}/>
+          <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to='/login'/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
